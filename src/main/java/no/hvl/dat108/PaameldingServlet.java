94987bf92e21105935f479233045c4afae961f01
Isak <595489@stud.hvl.no>
2021-11-13 16:08:24 +0100

    Initial commit

---
+++ src/main/java/no/hvl/dat108/PaameldingServlet.java
@@ -0,0 +1,27 @@
+package no.hvl.dat108;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PaameldingServlet")
+public class PaameldingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public PaameldingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
